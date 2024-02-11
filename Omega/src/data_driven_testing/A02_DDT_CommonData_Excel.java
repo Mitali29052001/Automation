28ceb2ff653776081e0ce9648fe547bc2eafd9bc
mitali <mitalikadam29052001@gmail.com>
2024-02-10 15:52:12 -1100

    first commit

---
+++ Omega/src/data_driven_testing/A02_DDT_CommonData_Excel.java
@@ -0,0 +1,40 @@
+//To use excel we need to download jar file
+//1)tinyurl.com/aliselnium-sir url
+//2)a)https://archive.apache.org/dist/poi/release/bin/ then go to 
+//2)b)poi-bin-5.2.3-20220909.zip 
+//after downloading zip file extract it
+//go to extracted folder copy all jar file except licence and notice file, auxilary, lib, ooxml-lib folder
+//paste it in eclipse omega projects jar folder.
+//again go to the auxilary, lib, ooxml-lib folder one by one and paste the all jar file into the eclispe omega project jar folder.
+//go to the jar file and copy jar file by doing shift+down key from  first jar file till before name like selenium jar file and click right click and select build path then click on add build path all jar file will go to the reference libraries.
+// again go to the jar file and copy jar file by doing shift+down key from after name like selenium jar file to last file and click right click and select build path then click on add build path all jar file will go to the reference libraries.
+////object create
+//1)FileinputStream file = new Fileinputstream("./TestData/commondata.property")
+//FileinputStream->java class which is coming from io package. file->R.V, = -> Assignment Operator which used to switch the right side data to the left side R.V. new->keyword. Fileinputstream("./testdata/commonproperty)->Constructor which accept one parameter as string path. "./TestData/commondata.property"-> string path
+//2)workbook wb = workbookfactory.create(file);
+//WorkBook->Return type of wb, wb->R.V,To get data we use reference variable, workbookfactory->java class come from jar, create(file)->static method in workbookfactory accept one parameter to load excel data
+//Workbookfactory->java class come from jar, it take complete control of fis data and switch to wb
+//3)wb.getsheet("studentdata").getRow(2).getcell(1).getStringcellvalue();
+//wb->R.V, getsheet("studentdata")->This getsheet() method get the control to specified sheet in excel which accept one parameter as sheet string name
+//getRow(2)->This getRow() method get the control to specified Row in excel data which accept one parameter as row number
+//getcell(1)->This getcell() method get the control to specified cell in excel data which accept one parameter as cell number
+//getStringcellvalue()->This getStringcellvalue() method picked the value from cell.
+//getNumericcellvalue()->It work same as getStringcellvalue, but this getNumericcellvalue() method is usd when we want picked number from cell then we use getNumbercellvalue() method, It picked the value from the cell which is in double datatype because in excel take number as 1.0 if we dont use this and used getStringcellvalue we get error.
+package data_driven_testing;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class A02_DDT_CommonData_Excel {
+	public static void main(String[] args) throws IOException {
+		FileInputStream file = new FileInputStream("./Testdata/TestData.xlsx");
+		Workbook wb = WorkbookFactory.create(file);
+		String data = wb.getSheet("SD").getRow(2).getCell(1).getStringCellValue();
+		System.out.println(data);
+		
+	}
+}
---
+++ Omega/src/data_driven_testing/A03_DDT_CD_UpdateData_Setcellvalue.java
@@ -0,0 +1,27 @@
+//If we want to update data
+//1)setcellvalue()->It is used when we want to update the data 
+//wb.getsheet("sd").getRow(2).getcell(3).setcellvalue('student');
+//2)Fileoutputstream file = new Fileoutputstream("./Testdata/TestData.xlsx.xls")
+//Fileoutputstream-> java class coming from io package which is used to show output, file->R.V in which we store all string path data 
+//new -> keyword, Fileoutputstream()->Constructor which accept one parameter as string path.
+//3)wb.write(file)
+//wb->R.V, write()->Its a method which is used to replace the old data with new data
+package data_driven_testing;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class A03_DDT_CD_UpdateData_Setcellvalue {
+	public static void main(String[] args) throws IOException {
+		FileInputStream file = new FileInputStream("./Testdata/TestData.xlsx");
+		Workbook wb = WorkbookFactory.create(file);
+		wb.getSheet("SD").getRow(2).getCell(2).setCellValue("Commerce");
+		FileOutputStream file1 = new FileOutputStream("./Testdata/TestData.xlsx");
+		wb.write(file1);
+	}
+}
